9211af29367d2d65dca544f124a32cf919305b7c
Patrick Thomson <patrickt@github.com>
2018-05-22 10:56:25 -0400

    Add support for cancelling an in-flight parse.
    Hooks into `ts_parser_enabled` and `ts_parser_set_enabled`. Bumps tree-sitter.

--- src/TreeSitter/Parser.hs
+++ src/TreeSitter/Parser.hs
@@ -14,4 +14,6 @@ foreign import ccall safe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_p
 foreign import ccall safe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_parser_parse_string" ts_parser_parse_string :: Ptr Parser -> Ptr Tree -> CString -> Int -> IO (Ptr Tree)
 foreign import ccall safe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_parser_delete" ts_parser_delete :: Ptr Parser -> IO ()
 foreign import ccall safe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_parser_set_language" ts_parser_set_language :: Ptr Parser -> Ptr Language -> IO ()
+foreign import ccall safe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_parser_enabled" ts_parser_enabled :: Ptr Parser -> IO CBool
+foreign import ccall safe "vendor/tree-sitter/include/tree_sitter/runtime.h ts_parser_set_enabled" ts_parser_set_enabled :: Ptr Parser -> CBool -> IO ()
 foreign import ccall safe "src/bridge.c ts_parser_log_to_stderr" ts_parser_log_to_stderr :: Ptr Parser -> IO ()
