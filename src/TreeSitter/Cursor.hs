b85d8816eef0281200c2c0a3f5ef7ed49ba28089
jwin <jwin1968@gmail.com>
2018-11-17 13:08:51 +0100

    refactor common type Struct

--- src/TreeSitter/Cursor.hs
+++ src/TreeSitter/Cursor.hs
@@ -16,6 +16,7 @@ import Foreign.C
 import GHC.Generics
 
 import TreeSitter.Tree
+import TreeSitter.Struct
 
 
 data Cursor = Cursor
@@ -49,48 +50,6 @@ instance Storable TSPoint where
                               <*> peekStruct
   poke _ _ = error "Cant poke"
 
-
--- | 'Struct' is a strict 'Monad' with automatic alignment & advancing, & inferred type.
-newtype Struct a = Struct { runStruct :: forall b . Ptr b -> IO (a, Ptr a) }
-
-evalStruct :: Struct a -> Ptr b -> IO a
-evalStruct s p = fmap fst $! runStruct s p
-{-# INLINE evalStruct #-}
-
-peekStruct :: forall a . Storable a => Struct a
-peekStruct = Struct (\ p -> do
-  let aligned = alignPtr (castPtr p) (alignment (undefined :: a))
-  a <- peek aligned
-  pure (a, aligned `plusPtr` sizeOf a))
-{-# INLINE peekStruct #-}
-
-instance Functor Struct where
-  fmap f a = Struct (\ p -> do
-    (a', p') <- runStruct a p
-    let fa = f a'
-    fa `seq` p' `seq` pure (fa, castPtr p'))
-  {-# INLINE fmap #-}
-
-instance Applicative Struct where
-  pure a = Struct (\ p -> pure (a, castPtr p))
-  {-# INLINE pure #-}
-
-  f <*> a = Struct (\ p -> do
-    (f', p')  <- runStruct f          p
-    (a', p'') <- p' `seq` runStruct a (castPtr p')
-    let fa = f' a'
-    fa `seq` p'' `seq` pure (fa, castPtr p''))
-  {-# INLINE (<*>) #-}
-
-instance Monad Struct where
-  return = pure
-  {-# INLINE return #-}
-
-  a >>= f = Struct (\ p -> do
-    (a', p')   <- runStruct a               p
-    (fa', p'') <- p' `seq` runStruct (f a') (castPtr p')
-    fa' `seq` p'' `seq` pure (fa', p''))
-  {-# INLINE (>>=) #-}
 
 
 data Navigation = Down | Next | Up
--- src/TreeSitter/Node.hs
+++ src/TreeSitter/Node.hs
@@ -11,6 +11,9 @@ import Foreign
 import Foreign.C
 import GHC.Generics
 
+import TreeSitter.Struct
+
+
 data Node = Node
   { nodeTSNode :: !TSNode
   , nodeType :: !CString
@@ -28,27 +31,6 @@ data TSPoint = TSPoint { pointRow :: !Word32, pointColumn :: !Word32 }
 
 data TSNode = TSNode !Word32 !Word32 !Word32 !Word32 !(Ptr ()) !(Ptr ())
   deriving (Show, Eq, Generic)
-
--- | 'Struct' is a strict 'Monad' with automatic alignment & advancing, & inferred type.
-newtype Struct a = Struct { runStruct :: forall b . Ptr b -> IO (a, Ptr a) }
-
-evalStruct :: Struct a -> Ptr b -> IO a
-evalStruct s p = fmap fst $! runStruct s p
-{-# INLINE evalStruct #-}
-
-peekStruct :: forall a . Storable a => Struct a
-peekStruct = Struct (\ p -> do
-  let aligned = alignPtr (castPtr p) (alignment (undefined :: a))
-  a <- peek aligned
-  pure (a, aligned `plusPtr` sizeOf a))
-{-# INLINE peekStruct #-}
-
-pokeStruct :: Storable a => a -> Struct ()
-pokeStruct a = Struct (\ p -> do
-  let aligned = alignPtr (castPtr p) (alignment a)
-  poke aligned a
-  pure ((), castPtr aligned `plusPtr` sizeOf a))
-{-# INLINE pokeStruct #-}
 
 
 instance Storable Node where
@@ -97,34 +79,5 @@ instance Storable TSNode where
     pokeStruct o4
     pokeStruct p1
     pokeStruct p2
-
-instance Functor Struct where
-  fmap f a = Struct (\ p -> do
-    (a', p') <- runStruct a p
-    let fa = f a'
-    fa `seq` p' `seq` pure (fa, castPtr p'))
-  {-# INLINE fmap #-}
-
-instance Applicative Struct where
-  pure a = Struct (\ p -> pure (a, castPtr p))
-  {-# INLINE pure #-}
-
-  f <*> a = Struct (\ p -> do
-    (f', p')  <- runStruct f          p
-    (a', p'') <- p' `seq` runStruct a (castPtr p')
-    let fa = f' a'
-    fa `seq` p'' `seq` pure (fa, castPtr p''))
-  {-# INLINE (<*>) #-}
-
-instance Monad Struct where
-  return = pure
-  {-# INLINE return #-}
-
-  a >>= f = Struct (\ p -> do
-    (a', p')   <- runStruct a               p
-    (fa', p'') <- p' `seq` runStruct (f a') (castPtr p')
-    fa' `seq` p'' `seq` pure (fa', p''))
-  {-# INLINE (>>=) #-}
-
 
 foreign import ccall interruptible "src/bridge.c ts_node_copy_child_nodes" ts_node_copy_child_nodes :: Ptr TSNode -> Ptr Node -> IO ()
