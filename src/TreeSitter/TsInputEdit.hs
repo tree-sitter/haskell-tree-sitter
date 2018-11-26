a9c69ab9365011b37e96510b91ed42eee73b2111
jwin <jwin1968@gmail.com>
2018-11-26 21:54:19 +0100

    add module for TSInputEdit

---
+++ src/TreeSitter/TsInputEdit.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass, InterruptibleFFI, RankNTypes, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -funbox-strict-fields #-}
+module TreeSitter.TsInputEdit (
+  TSInputEdit(..)
+) where
+
+import Foreign
+import GHC.Generics
+
+import TreeSitter.Struct
+import TreeSitter.TsPoint
+
+
+data TSInputEdit = TSInputEdit {
+    startByte :: !Word32
+    , oldEndByte :: !Word32
+    , newEndByte :: !Word32
+    , startPoint :: !TSPoint
+    , oldEndPoint :: !TSPoint
+    , newEndPoint :: !TSPoint
+  }
+  deriving (Show, Eq, Generic)
+
+instance Storable TSInputEdit where
+  alignment _ = alignment (0 :: Int32)
+  sizeOf _ = 8
+  peek = evalStruct $ TSInputEdit <$> peekStruct
+                                  <*> peekStruct
+                                  <*> peekStruct
+                                  <*> peekStruct
+                                  <*> peekStruct
+                                  <*> peekStruct
+  poke ptr (TSInputEdit sb oeb neb sp oep nep) = flip evalStruct ptr $ do
+    pokeStruct sb
+    pokeStruct oeb
+    pokeStruct neb
+    pokeStruct sp
+    pokeStruct oep
+    pokeStruct nep
+
