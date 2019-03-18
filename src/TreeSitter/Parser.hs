3996ccfbe7b2a9efbfae8acab034393dfe6937cf
Max Brunsfeld <maxbrunsfeld@gmail.com>
2019-03-18 12:11:15 -0700

    Remove header filenames from foreign import declarations
    Including the header filename in the declaration seems to be deprecated,
    according to the haskell wiki:
    
    https://wiki.haskell.org/Foreign_Function_Interface#Header_inclusion

--- src/TreeSitter/Parser.hs
+++ src/TreeSitter/Parser.hs
@@ -8,12 +8,12 @@ import TreeSitter.Tree
 newtype Parser = Parser ()
   deriving (Show, Eq)
 
-foreign import ccall safe "api.h ts_parser_new" ts_parser_new :: IO (Ptr Parser)
-foreign import ccall safe "api.h ts_parser_halt_on_error" ts_parser_halt_on_error :: Ptr Parser -> CBool -> IO ()
-foreign import ccall safe "api.h ts_parser_parse_string" ts_parser_parse_string :: Ptr Parser -> Ptr Tree -> CString -> Int -> IO (Ptr Tree)
-foreign import ccall safe "api.h ts_parser_delete" ts_parser_delete :: Ptr Parser -> IO ()
-foreign import ccall safe "api.h ts_parser_set_language" ts_parser_set_language :: Ptr Parser -> Ptr Language -> IO ()
-foreign import ccall safe "api.h ts_parser_timeout_micros" ts_parser_timeout_micros :: Ptr Parser -> IO Word64
-foreign import ccall safe "api.h ts_parser_set_timeout_micros" ts_parser_set_timeout_micros :: Ptr Parser -> Word64 -> IO ()
+foreign import ccall safe "ts_parser_new" ts_parser_new :: IO (Ptr Parser)
+foreign import ccall safe "ts_parser_halt_on_error" ts_parser_halt_on_error :: Ptr Parser -> CBool -> IO ()
+foreign import ccall safe "ts_parser_parse_string" ts_parser_parse_string :: Ptr Parser -> Ptr Tree -> CString -> Int -> IO (Ptr Tree)
+foreign import ccall safe "ts_parser_delete" ts_parser_delete :: Ptr Parser -> IO ()
+foreign import ccall safe "ts_parser_set_language" ts_parser_set_language :: Ptr Parser -> Ptr Language -> IO ()
+foreign import ccall safe "ts_parser_timeout_micros" ts_parser_timeout_micros :: Ptr Parser -> IO Word64
+foreign import ccall safe "ts_parser_set_timeout_micros" ts_parser_set_timeout_micros :: Ptr Parser -> Word64 -> IO ()
 
 foreign import ccall safe "src/bridge.c ts_parser_log_to_stderr" ts_parser_log_to_stderr :: Ptr Parser -> IO ()
--- src/TreeSitter/Tree.hs
+++ src/TreeSitter/Tree.hs
@@ -6,5 +6,5 @@ import TreeSitter.Node
 newtype Tree = Tree ()
   deriving (Show, Eq)
 
-foreign import ccall safe "api.h ts_tree_delete" ts_tree_delete :: Ptr Tree -> IO ()
+foreign import ccall safe "ts_tree_delete" ts_tree_delete :: Ptr Tree -> IO ()
 foreign import ccall unsafe "src/bridge.c ts_tree_root_node_p" ts_tree_root_node_p :: Ptr Tree -> Ptr Node -> IO ()
