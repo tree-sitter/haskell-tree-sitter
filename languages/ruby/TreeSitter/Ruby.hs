c96ac299a76880d8f474b81a844302eb52250ffa
Ayman Nadeem <aymannadeem@github.com>
2019-05-13 12:55:31 -0700

    revert Ruby back to original

--- languages/ruby/TreeSitter/Ruby.hs
+++ languages/ruby/TreeSitter/Ruby.hs
@@ -1,14 +1,8 @@
-{-# LANGUAGE TemplateHaskell, DuplicateRecordFields #-}
 module TreeSitter.Ruby
 ( tree_sitter_ruby
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
-import CodeGen.GenerateSyntax
-import Control.Monad.IO.Class
 
 foreign import ccall unsafe "vendor/tree-sitter-ruby/src/parser.c tree_sitter_ruby" tree_sitter_ruby :: Ptr Language
-
-$(input >>= liftIO . print >> pure [])
-$(input >>= traverse datatypeForConstructors)
