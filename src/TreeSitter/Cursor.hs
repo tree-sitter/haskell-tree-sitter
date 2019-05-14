fe74fc059615c3f88168b2342ce6c1be96acfaa6
Rob Rix <robrix@github.com>
2019-05-14 18:11:56 -0400

    Encode the size of TSTreeCursor.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/TreeSitter/Cursor.hs
+++ src/TreeSitter/Cursor.hs
@@ -5,6 +5,9 @@ import TreeSitter.Node
 
 data Cursor = Cursor
 
+sizeOfCursor :: Int
+sizeOfCursor = 72
+
 foreign import ccall unsafe "src/bridge.c ts_tree_cursor_new_p" ts_tree_cursor_new_p :: Ptr TSNode -> Ptr Cursor -> IO ()
 foreign import ccall unsafe "ts_tree_cursor_delete" ts_tree_cursor_delete :: Ptr Cursor -> IO ()
 
