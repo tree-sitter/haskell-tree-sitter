ce9b8d845fb5673efd42ad1bcbcb3a4ce3e35089
Rob Rix <robrix@github.com>
2019-05-14 17:20:20 -0400

    Export most of the tree-sitter cursor API.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- src/TreeSitter/Cursor.hs
+++ src/TreeSitter/Cursor.hs
@@ -1,3 +1,15 @@
 module TreeSitter.Cursor where
 
+import Foreign.Ptr
+import TreeSitter.Node
+
 data Cursor = Cursor
+
+foreign import ccall unsafe "src/bridge.c ts_tree_cursor_new_p" ts_tree_cursor_new_p :: Ptr TSNode -> Ptr Cursor -> IO ()
+foreign import ccall unsafe "ts_tree_cursor_delete" ts_tree_cursor_delete :: Ptr Cursor -> IO ()
+
+foreign import ccall unsafe "src/bridge.c ts_tree_cursor_current_node_p" ts_tree_cursor_current_node_p :: Ptr Cursor -> Ptr TSNode -> IO ()
+
+foreign import ccall unsafe "ts_tree_cursor_goto_parent" ts_tree_cursor_goto_parent :: Ptr Cursor -> IO Bool
+foreign import ccall unsafe "ts_tree_cursor_goto_next_sibling" ts_tree_cursor_goto_next_sibling :: Ptr Cursor -> IO Bool
+foreign import ccall unsafe "ts_tree_cursor_goto_first_child" ts_tree_cursor_goto_first_child :: Ptr Cursor -> IO Bool
