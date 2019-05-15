957a136466743fddf5546646d3f9df1d5e692abd
Rob Rix <robrix@github.com>
2019-05-15 09:53:34 -0400

    Export the current field name.

--- src/TreeSitter/Cursor.hs
+++ src/TreeSitter/Cursor.hs
@@ -1,5 +1,6 @@
 module TreeSitter.Cursor where
 
+import Foreign.C
 import Foreign.Ptr
 import TreeSitter.Node
 
@@ -12,6 +13,7 @@ foreign import ccall unsafe "src/bridge.c ts_tree_cursor_new_p" ts_tree_cursor_n
 foreign import ccall unsafe "ts_tree_cursor_delete" ts_tree_cursor_delete :: Ptr Cursor -> IO ()
 
 foreign import ccall unsafe "src/bridge.c ts_tree_cursor_current_node_p" ts_tree_cursor_current_node_p :: Ptr Cursor -> Ptr TSNode -> IO ()
+foreign import ccall unsafe "ts_tree_cursor_current_field_name" ts_tree_cursor_current_field_name :: Ptr Cursor -> IO CString
 
 foreign import ccall unsafe "ts_tree_cursor_goto_parent" ts_tree_cursor_goto_parent :: Ptr Cursor -> IO Bool
 foreign import ccall unsafe "ts_tree_cursor_goto_next_sibling" ts_tree_cursor_goto_next_sibling :: Ptr Cursor -> IO Bool
