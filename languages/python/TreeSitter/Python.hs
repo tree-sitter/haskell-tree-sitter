d20ca102f6f4e9169e236136ed6fe2df4e42cb90
Rob Rix <robrix@github.com>
2019-05-16 17:07:22 -0400

    :fire: a redundant import.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- languages/python/TreeSitter/Python.hs
+++ languages/python/TreeSitter/Python.hs
@@ -8,7 +8,6 @@ import TreeSitter.Language
 import CodeGen.Deserialize
 import CodeGen.GenerateSyntax
 import Control.Monad.IO.Class
-import Language.Haskell.TH
 import Data.Aeson as Aeson
 
 foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_python" tree_sitter_python :: Ptr Language
