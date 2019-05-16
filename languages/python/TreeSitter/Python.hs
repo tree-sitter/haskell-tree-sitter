07daa65cc65d5c99fa1985c7b52c39705e39d81d
Rob Rix <robrix@github.com>
2019-05-16 17:08:03 -0400

    Hide the imports of clashing symbols.
    Co-Authored-By: Ayman Nadeem <aymannadeem@gmail.com>

--- languages/python/TreeSitter/Python.hs
+++ languages/python/TreeSitter/Python.hs
@@ -9,6 +9,7 @@ import CodeGen.Deserialize
 import CodeGen.GenerateSyntax
 import Control.Monad.IO.Class
 import Data.Aeson as Aeson
+import Prelude hiding (Float, Integer, String)
 
 foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_python" tree_sitter_python :: Ptr Language
 
