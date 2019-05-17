24a7e48a0f27ac636b75511a931f5437a722b43d
Rob Rix <robrix@github.com>
2019-05-17 12:22:15 -0400

    :fire: the Eq instance for Tree.

--- src/TreeSitter/Tree.hs
+++ src/TreeSitter/Tree.hs
@@ -4,7 +4,7 @@ import Foreign
 import TreeSitter.Node
 
 data Tree = Tree
-  deriving (Show, Eq)
+  deriving (Show)
 
 foreign import ccall safe "ts_tree_delete" ts_tree_delete :: Ptr Tree -> IO ()
 foreign import ccall unsafe "src/bridge.c ts_tree_root_node_p" ts_tree_root_node_p :: Ptr Tree -> Ptr Node -> IO ()
