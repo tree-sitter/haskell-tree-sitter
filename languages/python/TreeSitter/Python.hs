41851a12a6d985841f3a29c988786a52edcf586f
Rob Rix <robrix@github.com>
2019-05-17 11:45:30 -0400

    Move parseByteString into the python executable, to reduce rebuilds of TreeSitter.Python.

--- languages/python/TreeSitter/Python.hs
+++ languages/python/TreeSitter/Python.hs
@@ -1,16 +1,11 @@
 {-# LANGUAGE DeriveAnyClass, DeriveGeneric, DuplicateRecordFields, TemplateHaskell #-}
 module TreeSitter.Python where
 
-import qualified Data.ByteString as B
-import qualified Control.Exception as Exc
 import Foreign.Ptr
-import qualified TreeSitter.Importing as TS
 import TreeSitter.Language
-import TreeSitter.Parser
-import CodeGen.Deserialize
 import CodeGen.GenerateSyntax
 import Control.Monad.IO.Class
-import Data.Aeson as Aeson
+import Data.Aeson
 import Prelude hiding (Float, Integer, String)
 
 foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_python" tree_sitter_python :: Ptr Language
@@ -19,12 +14,3 @@ foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_
 $(do
   input <- liftIO (eitherDecodeFileStrict' "./vendor/tree-sitter-python/src/node-types.json")
   either fail (traverse datatypeForConstructors) input)
-
-
-parseByteString :: B.ByteString -> IO (Maybe Module)
-parseByteString bytestring = Exc.bracket
-  ts_parser_new
-  ts_parser_delete
-  $ \ parser -> do
-    ts_parser_set_language parser tree_sitter_python
-    TS.parseByteString parser bytestring
--- languages/python/app/Main.hs
+++ languages/python/app/Main.hs
@@ -1,7 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Control.Exception as Exc
+import qualified Data.ByteString as B
+import qualified TreeSitter.Importing as TS
+import TreeSitter.Parser
 import TreeSitter.Python
+
+parseByteString :: B.ByteString -> IO (Maybe Module)
+parseByteString bytestring = Exc.bracket
+  ts_parser_new
+  ts_parser_delete
+  $ \ parser -> do
+    ts_parser_set_language parser tree_sitter_python
+    TS.parseByteString parser bytestring
 
 main :: IO ()
 main = do
