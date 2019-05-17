6b69ef74f9b8a32a96b1a381886ca97f387473f2
Rob Rix <robrix@github.com>
2019-05-17 12:24:18 -0400

    Define a helper to run an action with the root node of a tree.

--- src/TreeSitter/Tree.hs
+++ src/TreeSitter/Tree.hs
@@ -1,5 +1,6 @@
 module TreeSitter.Tree
 ( Tree
+, withRootNode
 , ts_tree_delete
 , ts_tree_root_node_p
 ) where
@@ -9,6 +10,11 @@ import TreeSitter.Node
 
 data Tree = Tree
   deriving (Show)
+
+withRootNode :: Ptr Tree -> (Ptr Node -> IO a) -> IO a
+withRootNode tree action = alloca $ \ ptr -> do
+  ts_tree_root_node_p tree ptr
+  action ptr
 
 foreign import ccall safe "ts_tree_delete" ts_tree_delete :: Ptr Tree -> IO ()
 foreign import ccall unsafe "src/bridge.c ts_tree_root_node_p" ts_tree_root_node_p :: Ptr Tree -> Ptr Node -> IO ()
