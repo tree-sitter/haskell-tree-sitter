a8f9ba1adb4f51c85526eb219090705678fc9251
Rob Rix <robrix@github.com>
2019-06-03 13:18:54 -0400

    Construct the JSON grammar datatype.

--- languages/json/TreeSitter/JSON.hs
+++ languages/json/TreeSitter/JSON.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
 module TreeSitter.JSON
 ( tree_sitter_json
 ) where
 
+import Language.Haskell.TH
 import TreeSitter.JSON.Internal
+import TreeSitter.Language
+
+-- Regenerate template haskell code when these files change:
+addDependentFileRelative "../vendor/tree-sitter-json/src/parser.c"
+
+-- | Statically-known rules corresponding to symbols in the grammar.
+mkSymbolDatatype (mkName "Grammar") tree_sitter_json
