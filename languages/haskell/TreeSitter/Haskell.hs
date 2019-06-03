47cadf173d4038b683feb9d6e57d615edbfbe3ff
Rob Rix <robrix@github.com>
2019-06-03 14:07:02 -0400

    Do the thing for Haskell.

--- languages/haskell/TreeSitter/Haskell.hs
+++ languages/haskell/TreeSitter/Haskell.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
 module TreeSitter.Haskell
 ( tree_sitter_haskell
+, Grammar(..)
 ) where
 
-import Foreign.Ptr
+import Language.Haskell.TH
+import TreeSitter.Haskell.Internal
 import TreeSitter.Language
 
-foreign import ccall unsafe "vendor/tree-sitter-haskell/src/parser.c tree_sitter_haskell" tree_sitter_haskell :: Ptr Language
+-- Regenerate template haskell code when these files change:
+addDependentFileRelative "../vendor/tree-sitter-haskell/src/parser.c"
+
+-- | Statically-known rules corresponding to symbols in the grammar.
+mkSymbolDatatype (mkName "Grammar") tree_sitter_haskell
