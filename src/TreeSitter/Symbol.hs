f96a1c5b209751e435092417cdea97d1a222f030
Rob Rix <robrix@github.com>
2019-06-21 10:29:49 -0400

    Define a helper to convert a Word16 to a symbol.

--- src/TreeSitter/Symbol.hs
+++ src/TreeSitter/Symbol.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module TreeSitter.Symbol where
+
+import Data.Word
+import TreeSitter.Language
+
+toSymbol :: forall symbol . Symbol symbol => Word16 -> symbol
+toSymbol symbol = toEnum (min (fromIntegral symbol) (fromEnum (maxBound :: symbol)))
