9f5f68275b03297e93521b6612d23b08347487ba
Ayman Nadeem <aymannadeem@github.com>
2019-08-07 16:39:18 -0400

    unmarshal this by supplying SymbolMatching instances with typeParameterName

--- tree-sitter/src/TreeSitter/GenerateSyntax.hs
+++ tree-sitter/src/TreeSitter/GenerateSyntax.hs
@@ -50,20 +50,19 @@ astDeclarationsForLanguage language filePath = do
 syntaxDatatype :: Ptr TS.Language -> Datatype -> Q [Dec]
 syntaxDatatype language datatype = case datatype of
   SumType (DatatypeName datatypeName) _ subtypes -> do
-    cons <- traverse (constructorForSumChoice datatypeName) subtypes
-    result <- symbolMatchingInstanceForSums language name subtypes
-    pure $ generatedDatatype name cons:result
     typeParameterName <- newName "a"
+    cons <- traverse (constructorForSumChoice datatypeName typeParameterName) subtypes
+    result <- symbolMatchingInstanceForSums language name subtypes typeParameterName
     pure $ generatedDatatype name cons typeParameterName:result
   ProductType (DatatypeName datatypeName) _ children fields -> do
-    con <- ctorForProductType datatypeName children fields
-    result <- symbolMatchingInstance language name datatypeName
-    pure $ generatedDatatype name [con]:result
     typeParameterName <- newName "a"
+    con <- ctorForProductType datatypeName typeParameterName children fields
+    result <- symbolMatchingInstance language name datatypeName typeParameterName
+    pure $ generatedDatatype name [con] typeParameterName:result
   LeafType (DatatypeName datatypeName) named -> do
     typeParameterName <- newName "a"
     con <- ctorForLeafType named (DatatypeName datatypeName)
-    result <- symbolMatchingInstance language name datatypeName
+    result <- symbolMatchingInstance language name datatypeName typeParameterName
     pure $ case named of
       Anonymous -> generatedDatatype name [con] typeParameterName:result
       Named -> NewtypeD [] name [PlainTV typeParameterName] Nothing con deriveClause:result
@@ -74,40 +73,40 @@ syntaxDatatype language datatype = case datatype of
 
 
 -- | Create TH-generated SymbolMatching instances for sums, products, leaves
-symbolMatchingInstance :: Ptr TS.Language -> Name -> String -> Q [Dec]
-symbolMatchingInstance language name str = do
+symbolMatchingInstance :: Ptr TS.Language -> Name -> String -> Name -> Q [Dec]
+symbolMatchingInstance language name str typeParameterName = do
   tsSymbol <- runIO $ withCString str (pure . TS.ts_language_symbol_for_name language)
   let tsSymbolType = toEnum $ TS.ts_language_symbol_type language tsSymbol
-  [d|instance TS.SymbolMatching $(conT name) where
+  [d|instance TS.SymbolMatching $(appT (conT name) (varT typeParameterName)) where
       showFailure _ node = "Expected " <> $(litE (stringL (show name))) <> " but got " <> show (TS.fromTSSymbol (nodeSymbol node) :: $(conT (mkName "Grammar.Grammar")))
       symbolMatch _ node = TS.fromTSSymbol (nodeSymbol node) == $(conE (mkName $ "Grammar." <> TS.symbolToName tsSymbolType str))|]
 
-symbolMatchingInstanceForSums ::  Ptr TS.Language -> Name -> [TreeSitter.Deserialize.Type] -> Q [Dec]
-symbolMatchingInstanceForSums _ name subtypes =
-  [d|instance TS.SymbolMatching $(conT name) where
+symbolMatchingInstanceForSums ::  Ptr TS.Language -> Name -> [TreeSitter.Deserialize.Type] -> Name -> Q [Dec]
+symbolMatchingInstanceForSums _ name subtypes typeParameterName =
+  [d|instance TS.SymbolMatching $(appT (conT name) (varT typeParameterName)) where
       showFailure _ node = "Expected " <> $(litE (stringL (show (map extractn subtypes)))) <> " but got " <> show (TS.fromTSSymbol (nodeSymbol node) :: $(conT (mkName "Grammar.Grammar")))
       symbolMatch _ = $(foldr1 mkOr (perMkType `map` subtypes)) |]
-  where perMkType (MkType (DatatypeName n) named) = [e|TS.symbolMatch (Proxy :: Proxy $(conT (toName named n))) |]
+  where perMkType (MkType (DatatypeName n) named) = [e|TS.symbolMatch (Proxy :: Proxy $(appT (conT (toName named n)) (varT typeParameterName))) |]
         mkOr lhs rhs = [e| (||) <$> $(lhs) <*> $(rhs) |]
         extractn (MkType (DatatypeName n) Named) = toCamelCase n
         extractn (MkType (DatatypeName n) Anonymous) = "Anonymous" <> toCamelCase n
 
 
 -- | Append string with constructor name (ex., @IfStatementStatement IfStatement@)
-constructorForSumChoice :: String -> TreeSitter.Deserialize.Type -> Q Con
-constructorForSumChoice str (MkType (DatatypeName n) named) = normalC (toName named (n ++ str)) [child]
-  where child = TH.bangType (TH.bang noSourceUnpackedness noSourceStrictness) (conT (toName named n))
+constructorForSumChoice :: String -> Name -> TreeSitter.Deserialize.Type -> Q Con
+constructorForSumChoice str typeParameterName (MkType (DatatypeName n) named) = normalC (toName named (n ++ str)) [child]
+  where child = TH.bangType (TH.bang noSourceUnpackedness noSourceStrictness) (appT (conT (toName named n)) (varT typeParameterName))
 
 -- | Build Q Constructor for product types (nodes with fields)
-ctorForProductType :: String -> Maybe Children -> [(String, Field)] -> Q Con
-ctorForProductType constructorName children fields = recC (toName Named constructorName) lists where
+ctorForProductType :: String -> Name -> Maybe Children -> [(String, Field)] -> Q Con
+ctorForProductType constructorName typeParameterName children fields = recC (toName Named constructorName) lists where
   lists = fieldList ++ childList
   fieldList = fmap (uncurry toVarBangType) fields
   childList = toList $ fmap toVarBangTypeChild children
   toVarBangType name (MkField required fieldTypes mult) =
     let fieldName = mkName . addTickIfNecessary . removeUnderscore $ name
         strictness = TH.bang noSourceUnpackedness noSourceStrictness
-        ftypes = fieldTypesToNestedEither fieldTypes
+        ftypes = fieldTypesToNestedEither fieldTypes typeParameterName
         fieldContents = case (required, mult) of
           (Required, Multiple) -> appT (conT ''NonEmpty) ftypes
           (Required, Single) -> ftypes
@@ -125,11 +124,12 @@ ctorForLeafType Named (DatatypeName name) = recC (toName Named name) [leafBytes]
   textValue = TH.bangType (TH.bang noSourceUnpackedness noSourceStrictness) (conT ''Text)
 
 -- | Convert field types to Q types
-fieldTypesToNestedEither :: NonEmpty TreeSitter.Deserialize.Type -> Q TH.Type
-fieldTypesToNestedEither xs = foldr1 combine $ fmap convertToQType xs
+fieldTypesToNestedEither :: NonEmpty TreeSitter.Deserialize.Type -> Name -> Q TH.Type
+fieldTypesToNestedEither xs typeParameterName = foldr1 combine $ fmap convertToQType xs
   where
     combine convertedQType = appT (appT (conT ''Either) convertedQType)
-    convertToQType (MkType (DatatypeName n) named) = conT (toName named n)
+    convertToQType (MkType (DatatypeName n) named) = appT (conT (toName named n)) (varT typeParameterName)
+    -- TODO: pull convertToQType out to top-level fn
 
 -- | Convert snake_case string to CamelCase String
 toCamelCase :: String -> String
