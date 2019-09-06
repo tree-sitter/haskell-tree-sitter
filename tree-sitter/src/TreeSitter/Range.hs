669f387305a5db5b9ffc6181ced247bdf172139b
Ayman Nadeem <aymannadeem@github.com>
2019-09-06 13:11:24 -0400

    fix weird indenting

--- tree-sitter/src/TreeSitter/Range.hs
+++ tree-sitter/src/TreeSitter/Range.hs
@@ -2,6 +2,6 @@ module TreeSitter.Range
 ( Range(..)
 ) where
 
-  -- | A half-open interval of integers, defined by start & end indices.
-  data Range = Range { start :: {-# UNPACK #-} !Int, end :: {-# UNPACK #-} !Int }
-    deriving (Eq, Show)
+-- | A half-open interval of integers, defined by start & end indices.
+data Range = Range { start :: {-# UNPACK #-} !Int, end :: {-# UNPACK #-} !Int }
+  deriving (Eq, Show)
