7a60a1e4163597f843e3d8b4fa36cc5f9d14ee4d
Ayman Nadeem <aymannadeem@github.com>
2019-09-09 16:28:38 -0400

    address merge conflict for sequencing functionality in IO

--- tree-sitter/src/TreeSitter/GenerateSyntax.hs
+++ tree-sitter/src/TreeSitter/GenerateSyntax.hs
@@ -74,8 +74,8 @@ syntaxDatatype language datatype = do
 -- | Create TH-generated SymbolMatching instances for sums, products, leaves
 symbolMatchingInstance :: Ptr TS.Language -> Name -> String -> Name -> Q [Dec]
 symbolMatchingInstance language name str typeParameterName = do
-  tsSymbol <- runIO $ withCString str (pure . TS.ts_language_symbol_for_name language)
-  let tsSymbolType = toEnum $ TS.ts_language_symbol_type language tsSymbol
+  tsSymbol <- runIO $ withCString str (TS.ts_language_symbol_for_name language)
+  tsSymbolType <- toEnum <$> runIO (TS.ts_language_symbol_type language tsSymbol)
   [d|instance TS.SymbolMatching $(appT (conT name) (varT typeParameterName)) where
       showFailure _ node = "Expected " <> $(litE (stringL (show name))) <> " but got " <> show (TS.fromTSSymbol (nodeSymbol node) :: $(conT (mkName "Grammar.Grammar")))
       symbolMatch _ node = TS.fromTSSymbol (nodeSymbol node) == $(conE (mkName $ "Grammar." <> TS.symbolToName tsSymbolType str))|]
