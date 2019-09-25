af1a2faae4e4d4ef6c4dd0bbf1e5020f4bfd2077
Rob Rix <rob.rix@me.com>
2019-09-25 15:13:31 -0400

    Add some datatypes as examples of what we want to be able to unmarshal.

--- tree-sitter/src/TreeSitter/Unmarshal/Examples.hs
+++ tree-sitter/src/TreeSitter/Unmarshal/Examples.hs
@@ -1,1 +1,58 @@
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric, DuplicateRecordFields, TypeOperators #-}
 module TreeSitter.Unmarshal.Examples () where
+
+import qualified Data.Text as Text
+import GHC.Generics ((:+:), Generic1)
+import TreeSitter.Unmarshal
+
+data Stmt a
+  = IfStmt (If a)
+  | BlockStmt (Block a)
+  deriving (Generic1, Unmarshal)
+
+data If a = If { ann :: a, condition :: Expr a, consequence :: Stmt a, alternative :: Stmt a }
+  deriving (Generic1, Unmarshal)
+
+instance SymbolMatching If where
+  symbolMatch _ _ = False
+  showFailure _ _ = ""
+
+data Block a = Block { ann :: a, body :: [Stmt a] }
+  deriving (Generic1, Unmarshal)
+
+instance SymbolMatching Block where
+  symbolMatch _ _ = False
+  showFailure _ _ = ""
+
+data Expr a
+  = LitExpr (Lit a)
+  | BinExpr (Bin a)
+  deriving (Generic1, Unmarshal)
+
+data Lit a = Lit { ann :: a, text :: Text.Text }
+  deriving (Generic1, Unmarshal)
+
+instance SymbolMatching Lit where
+  symbolMatch _ _ = False
+  showFailure _ _ = ""
+
+data Bin a = Bin { ann :: a, lhs :: Expr a, op :: (AnonPlus :+: AnonTimes) a, rhs :: Expr a }
+  deriving (Generic1, Unmarshal)
+
+instance SymbolMatching Bin where
+  symbolMatch _ _ = False
+  showFailure _ _ = ""
+
+newtype AnonPlus a = AnonPlus { ann :: a }
+  deriving (Generic1, Unmarshal)
+
+instance SymbolMatching AnonPlus where
+  symbolMatch _ _ = False
+  showFailure _ _ = ""
+
+newtype AnonTimes a = AnonTimes { ann :: a }
+  deriving (Generic1, Unmarshal)
+
+instance SymbolMatching AnonTimes where
+  symbolMatch _ _ = False
+  showFailure _ _ = ""
