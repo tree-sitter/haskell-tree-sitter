1092774b2d9d3e32a6244d945fdf155bf954efc9
Rob Rix <rob.rix@me.com>
2019-09-25 15:32:51 -0400

    Define the SymbolMatching &c instances at * -> *.

--- tree-sitter/src/TreeSitter/Unmarshal/Examples.hs
+++ tree-sitter/src/TreeSitter/Unmarshal/Examples.hs
@@ -2,7 +2,7 @@
 module TreeSitter.Unmarshal.Examples () where
 
 import qualified Data.Text as Text
-import GHC.Generics ((:+:), Generic)
+import GHC.Generics ((:+:), Generic1)
 import TreeSitter.Unmarshal
 
 -- | An example of a sum-of-products datatype.
@@ -11,52 +11,52 @@ data Expr a
   | BlockExpr (Block a)
   | LitExpr (Lit a)
   | BinExpr (Bin a)
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
 -- | Product with multiple fields.
 data If a = If { ann :: a, condition :: Expr a, consequence :: Expr a, alternative :: Maybe (Expr a) }
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
-instance SymbolMatching (If a) where
+instance SymbolMatching If where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 -- | Single-field product.
 data Block a = Block { ann :: a, body :: [Expr a] }
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
-instance SymbolMatching (Block a) where
+instance SymbolMatching Block where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 -- | Leaf node.
 data Lit a = Lit { ann :: a, text :: Text.Text }
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
-instance SymbolMatching (Lit a) where
+instance SymbolMatching Lit where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 -- | Product with anonymous sum field.
 data Bin a = Bin { ann :: a, lhs :: Expr a, op :: (AnonPlus :+: AnonTimes) a, rhs :: Expr a }
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
-instance SymbolMatching (Bin a) where
+instance SymbolMatching Bin where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 -- | Anonymous leaf node.
 newtype AnonPlus a = AnonPlus { ann :: a }
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
-instance SymbolMatching (AnonPlus a) where
+instance SymbolMatching AnonPlus where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 -- | Anonymous leaf node.
 newtype AnonTimes a = AnonTimes { ann :: a }
-  deriving (Generic, Unmarshal)
+  deriving (Generic1, Unmarshal)
 
-instance SymbolMatching (AnonTimes a) where
+instance SymbolMatching AnonTimes where
   symbolMatch _ _ = False
   showFailure _ _ = ""
