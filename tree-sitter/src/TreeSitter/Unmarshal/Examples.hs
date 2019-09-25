8925ad9e08f49173a747560684839f73c71eec7e
Rob Rix <rob.rix@me.com>
2019-09-25 15:17:41 -0400

    *

--- tree-sitter/src/TreeSitter/Unmarshal/Examples.hs
+++ tree-sitter/src/TreeSitter/Unmarshal/Examples.hs
@@ -2,7 +2,7 @@
 module TreeSitter.Unmarshal.Examples () where
 
 import qualified Data.Text as Text
-import GHC.Generics ((:+:), Generic1)
+import GHC.Generics ((:+:), Generic)
 import TreeSitter.Unmarshal
 
 data Expr a
@@ -10,46 +10,46 @@ data Expr a
   | BlockExpr (Block a)
   | LitExpr (Lit a)
   | BinExpr (Bin a)
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
 data If a = If { ann :: a, condition :: Expr a, consequence :: Expr a, alternative :: Expr a }
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
-instance SymbolMatching If where
+instance SymbolMatching (If a) where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 data Block a = Block { ann :: a, body :: [Expr a] }
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
-instance SymbolMatching Block where
+instance SymbolMatching (Block a) where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 data Lit a = Lit { ann :: a, text :: Text.Text }
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
-instance SymbolMatching Lit where
+instance SymbolMatching (Lit a) where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 data Bin a = Bin { ann :: a, lhs :: Expr a, op :: (AnonPlus :+: AnonTimes) a, rhs :: Expr a }
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
-instance SymbolMatching Bin where
+instance SymbolMatching (Bin a) where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 newtype AnonPlus a = AnonPlus { ann :: a }
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
-instance SymbolMatching AnonPlus where
+instance SymbolMatching (AnonPlus a) where
   symbolMatch _ _ = False
   showFailure _ _ = ""
 
 newtype AnonTimes a = AnonTimes { ann :: a }
-  deriving (Generic1, Unmarshal)
+  deriving (Generic, Unmarshal)
 
-instance SymbolMatching AnonTimes where
+instance SymbolMatching (AnonTimes a) where
   symbolMatch _ _ = False
   showFailure _ _ = ""
