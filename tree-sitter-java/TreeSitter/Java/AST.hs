27e8b0c1c480bde1e0bce503b6073bd9654e12fd
Rob Rix <rob.rix@me.com>
2019-09-27 20:07:48 -0400

    Don’t import this.

--- tree-sitter-java/TreeSitter/Java/AST.hs
+++ tree-sitter-java/TreeSitter/Java/AST.hs
@@ -4,6 +4,5 @@ module TreeSitter.Java.AST where
 import TreeSitter.GenerateSyntax
 import Prelude hiding (True, False, Float, Integer, String)
 import qualified TreeSitter.Java as Grammar
-import Language.Haskell.TH
 
 astDeclarationsForLanguage Grammar.tree_sitter_java "../../vendor/tree-sitter-java/src/node-types.json"
