5f0f90c008ea2cfebba2e210633d4a7f70996388
Rob Rix <rob.rix@me.com>
2019-09-30 10:31:31 -0400

    Derive a bunch of instances for Token.

--- tree-sitter/src/TreeSitter/Token.hs
+++ tree-sitter/src/TreeSitter/Token.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE DeriveGeneric, DeriveTraversable #-}
 module TreeSitter.Token
 ( Token(..)
 ) where
 
+import GHC.Generics (Generic, Generic1)
+
 newtype Token sym a = Token { ann :: a }
+  deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable)
