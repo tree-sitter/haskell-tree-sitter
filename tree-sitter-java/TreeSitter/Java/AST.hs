b2f575f78153d6953d91f69da3eb7c3b6e240164
Rob Rix <rob.rix@me.com>
2019-10-02 20:19:03 -0400

    Don’t bother hiding Prelude symbols.

--- tree-sitter-java/TreeSitter/Java/AST.hs
+++ tree-sitter-java/TreeSitter/Java/AST.hs
@@ -4,7 +4,6 @@ module TreeSitter.Java.AST
 ) where
 
 import TreeSitter.GenerateSyntax
-import Prelude hiding (True, False, Float, Integer, String)
 import qualified TreeSitter.Java as Grammar
 
 astDeclarationsForLanguage Grammar.tree_sitter_java "../../vendor/tree-sitter-java/src/node-types.json"
