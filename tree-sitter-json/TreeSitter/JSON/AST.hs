5441bea480c0911882dbcca15e154d3539159cf3
Rob Rix <rob.rix@me.com>
2019-10-03 16:15:39 -0400

    Special-case StringContent.

--- tree-sitter-json/TreeSitter/JSON/AST.hs
+++ tree-sitter-json/TreeSitter/JSON/AST.hs
@@ -3,8 +3,17 @@ module TreeSitter.JSON.AST
 ( module TreeSitter.JSON.AST
 ) where
 
+import qualified Data.Text as Text
+import GHC.Generics (Generic, Generic1)
 import Prelude hiding (String)
 import TreeSitter.GenerateSyntax
 import qualified TreeSitter.JSON as Grammar
+import qualified TreeSitter.Unmarshal as TS
+
+data StringContent a = StringContent
+  { ann :: a
+  , text :: Text.Text
+  }
+  deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable, TS.Unmarshal)
 
 astDeclarationsForLanguage Grammar.tree_sitter_json "../../vendor/tree-sitter-json/src/node-types.json"
