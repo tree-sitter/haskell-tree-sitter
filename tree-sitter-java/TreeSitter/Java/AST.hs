321e419daa27dbeaaf074c913884566f5a1eb482
Ayman Nadeem <aymannadeem@github.com>
2019-10-07 17:09:04 -0400

    reformat language pragmas for Java

--- tree-sitter-java/TreeSitter/Java/AST.hs
+++ tree-sitter-java/TreeSitter/Java/AST.hs
@@ -1,4 +1,13 @@
-{-# LANGUAGE DataKinds, DeriveAnyClass, DeriveGeneric, DeriveTraversable, DuplicateRecordFields, TemplateHaskell, TypeApplications, GeneralizedNewtypeDeriving, DerivingStrategies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+
 module TreeSitter.Java.AST
 ( module TreeSitter.Java.AST
 ) where
