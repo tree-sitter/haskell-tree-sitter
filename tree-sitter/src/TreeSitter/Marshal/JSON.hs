fd9dcb341f5cf11a203549c3888788fc04932723
Ayman Nadeem <aymannadeem@github.com>
2019-11-25 14:26:08 -0500

    define default definition for marshal

--- tree-sitter/src/TreeSitter/Marshal/JSON.hs
+++ tree-sitter/src/TreeSitter/Marshal/JSON.hs
@@ -9,3 +9,5 @@ import GHC.Generics
 -- Typeclass to generically marshal ASTs into JSON
 class MarshalJSON t where
   marshal :: t a -> Value
+  default marshal :: ( Generic1 t, GMarshalJSON (Rep1 t)) => t a -> Value
+  marshal = gmarshal . from1
