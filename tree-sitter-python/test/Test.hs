a09d4caa1664ca96416ccade9bcc3afafa6ba274
Patrick Thomson <patrickt@github.com>
2019-12-10 16:07:37 -0500

    Ensure tree-sitter-python tests can be run with `cabal run`.
    `cabal test` moves into the subproject's folder before running,
    whereas `cabal run` doesn't. We can fix this by conditionally entering
    the `tree-sitter-python` directory.

--- tree-sitter-python/test/Test.hs
+++ tree-sitter-python/test/Test.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE DisambiguateRecordFields, OverloadedStrings, TypeApplications #-}
 module Main (main) where
 
+import           Control.Monad
+import           System.Path ((</>))
 import qualified System.Path as Path
+import           System.Path.Directory
 import           Test.Tasty
 import           TreeSitter.Python
 import qualified TreeSitter.Python.AST as Py
@@ -9,11 +12,15 @@ import           TreeSitter.Test.Helpers
 import           TreeSitter.Unmarshal
 
 main :: IO ()
-main
-  =   readCorpusFiles (Path.relDir "./vendor/tree-sitter-python/corpus")
-  >>= traverse (testCorpus parse)
-  >>= defaultMain . tests
-  where parse = parseByteString @Py.Module @() tree_sitter_python
+main = do
+  dir <- getCurrentDirectory
+  when (Path.takeDirName dir == Just (Path.relDir "haskell-tree-sitter")) $
+    setCurrentDirectory (dir </> Path.relDir "tree-sitter-python")
+  let parse = parseByteString @Py.Module @() tree_sitter_python
+  readCorpusFiles (Path.relDir "./vendor/tree-sitter-python/corpus")
+    >>= traverse (testCorpus parse)
+    >>= defaultMain . tests
+  where
 
 tests :: [TestTree] -> TestTree
 tests = testGroup "tree-sitter-python corpus tests"
