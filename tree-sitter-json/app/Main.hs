f316f1a61212b21b43f191f0d2698c1b71f5aaca
Rob Rix <rob.rix@me.com>
2019-12-12 14:08:55 -0500

    Run the Q action and print the datatypes.

--- tree-sitter-json/app/Main.hs
+++ tree-sitter-json/app/Main.hs
@@ -2,5 +2,12 @@ module Main
 ( main
 ) where
 
+import Language.Haskell.TH
+import TreeSitter.GenerateSyntax
+import TreeSitter.JSON.Internal
+
 main :: IO ()
-main = pure ()
+main = do
+  let jsonPath = "vendor/tree-sitter-json/src/node-types.json"
+  ast <- runQ (astDeclarationsForLanguage tree_sitter_json jsonPath)
+  putStrLn (pprint ast)
