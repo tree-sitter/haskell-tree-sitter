cc217200ab52fff3df63329a518e0adda2cb7a1b
Rob Rix <rob.rix@me.com>
2019-12-13 11:11:37 -0500

    Tidy the output with th-pprint.

--- tree-sitter-json/app/Main.hs
+++ tree-sitter-json/app/Main.hs
@@ -3,18 +3,15 @@ module Main
 ( main
 ) where
 
-import Data.List (intersperse)
-import Language.Haskell.TH
+import Language.Haskell.TH.Cleanup
 import TreeSitter.GenerateSyntax
 import TreeSitter.JSON.Internal
 import TreeSitter.JSON.AST.Internal
 
 main :: IO ()
 main = do
-  let jsonPath = "tree-sitter-json/vendor/tree-sitter-json/src/node-types.json"
-  ast <- runQ (astDeclarationsForLanguage tree_sitter_json [''StringContent] jsonPath)
   putStrLn moduleHeader
-  putStrLn (unlines (intersperse "" (map pprint ast)))
+  putStrLn $(simplifiedTH =<< astDeclarationsForLanguage tree_sitter_json [''StringContent] "/Users/rob/Developer/GitHub/haskell-tree-sitter/tree-sitter-json/vendor/tree-sitter-json/src/node-types.json")
 
 moduleHeader :: String
 moduleHeader = unlines
