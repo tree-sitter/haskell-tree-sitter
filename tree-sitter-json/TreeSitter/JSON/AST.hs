1a09f5ece965aa0aeb7c27ff4026d1925a066a6c
Timothy Clem <timothy.clem@gmail.com>
2020-01-08 10:30:08 -0800

    Just use generated StringContent for JSON
    Co-Authored-By: Rob Rix <rob.rix@github.com>

--- tree-sitter-json/TreeSitter/JSON/AST.hs
+++ tree-sitter-json/TreeSitter/JSON/AST.hs
@@ -10,26 +10,10 @@
 
 module TreeSitter.JSON.AST
 ( module TreeSitter.JSON.AST
-, (:+:)(..)
 ) where
 
-import qualified Data.Text as Text
-import GHC.Generics (Generic, Generic1, (:+:)(..))
-import Prelude hiding (String)
-import TreeSitter.GenerateSyntax
+import           Prelude hiding (String)
+import           TreeSitter.GenerateSyntax
 import qualified TreeSitter.JSON as Grammar
-import qualified TreeSitter.Unmarshal as TS
-
--- | The content of a string literal.
---
--- This is defined rather than being derived from the grammar to represent it as just the textual content, without explicit representation of escape sequences.
-data StringContent a = StringContent
-  { ann  :: a
-  , text :: Text.Text
-  }
-  deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable, TS.Unmarshal)
-
-instance TS.SymbolMatching StringContent where
-  showFailure _ _ = ""
 
 astDeclarationsForLanguage Grammar.tree_sitter_json "../../vendor/tree-sitter-json/src/node-types.json"
