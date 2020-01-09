4ed198eddefe64c8473702bb8022234bf7db222e
Rob Rix <rob.rix@me.com>
2020-01-09 13:24:47 -0500

    getArgs, not args.

--- tree-sitter-python/bench/Bench.hs
+++ tree-sitter-python/bench/Bench.hs
@@ -12,7 +12,7 @@ import qualified TreeSitter.Python.AST as Py
 import           TreeSitter.Unmarshal
 
 main :: IO ()
-main = args >>= defaultMain . map (bench <*> nfIO . parseFile)
+main = getArgs >>= defaultMain . map (bench <*> nfIO . parseFile)
 
 parseFile :: FilePath -> IO ()
 parseFile = either die pure <=< parseByteString @Py.Module @() tree_sitter_python <=< B.readFile
