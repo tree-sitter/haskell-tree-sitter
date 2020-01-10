0e0ecf42c39d2380c856e0a7bdd6744db4c6f1ff
Rob Rix <rob.rix@me.com>
2020-01-10 13:14:41 -0500

    :fire: a redundant FIXME.

--- tree-sitter/src/bridge.c
+++ tree-sitter/src/bridge.c
@@ -91,7 +91,6 @@ void ts_tree_cursor_reset_p(TSTreeCursor *cursor, TSNode *node) {
   ts_tree_cursor_reset(cursor, *node);
 }
 
-/* FIXME: copy out a Node rather than a TSNode */
 bool ts_tree_cursor_current_node_p(const TSTreeCursor *cursor, Node *outNode) {
   assert(cursor != NULL);
   assert(outNode != NULL);
