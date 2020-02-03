68eee849c978171ec6947b85f44da8405c4389cb
Ayman Nadeem <aymannadeem@github.com>
2020-02-03 16:09:34 -0500

    move contents of TreeSitter.Haskell.Internal to TreeSitter.Haskell

--- tree-sitter-haskell/TreeSitter/Haskell.hs
+++ tree-sitter-haskell/TreeSitter/Haskell.hs
@@ -1,15 +1,8 @@
-{-# LANGUAGE TemplateHaskell #-}
 module TreeSitter.Haskell
 ( tree_sitter_haskell
-, Grammar(..)
 ) where
 
-import Language.Haskell.TH
-import TreeSitter.Haskell.Internal
+import Foreign.Ptr
 import TreeSitter.Language
 
--- Regenerate template haskell code when these files change:
-addDependentFileRelative "../vendor/tree-sitter-haskell/src/parser.c"
-
--- | Statically-known rules corresponding to symbols in the grammar.
-mkSymbolDatatype (mkName "Grammar") tree_sitter_haskell
+foreign import ccall unsafe "vendor/tree-sitter-haskell/src/parser.c tree_sitter_haskell" tree_sitter_haskell :: Ptr Language
