8bfe1e7c5d4a2b1a4cefd04b5d6f906457b12a72
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:52:37 -0500

    add getNodeTypesPath to Python

--- tree-sitter-python/TreeSitter/Python.hs
+++ tree-sitter-python/TreeSitter/Python.hs
@@ -1,8 +1,13 @@
 module TreeSitter.Python
 ( tree_sitter_python
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_python
 
 foreign import ccall unsafe "vendor/tree-sitter-python/src/parser.c tree_sitter_python" tree_sitter_python :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-Python/Python/src/node-types.json"
