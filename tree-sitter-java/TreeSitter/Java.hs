ed60399ff157dd4c513424929628ee10eda2bada
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:56:40 -0500

    add getNodeTypesPath to Java

--- tree-sitter-java/TreeSitter/Java.hs
+++ tree-sitter-java/TreeSitter/Java.hs
@@ -1,8 +1,13 @@
 module TreeSitter.Java
 ( tree_sitter_java
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_java
 
 foreign import ccall unsafe "vendor/tree-sitter-java/src/parser.c tree_sitter_java" tree_sitter_java :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-Java/Java/src/node-types.json"
