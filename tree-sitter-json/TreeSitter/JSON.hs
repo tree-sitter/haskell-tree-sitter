cc6af5e97978c2e428f610996d8369a537653d13
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:57:58 -0500

    add getNodeTypesPath to JSON

--- tree-sitter-json/TreeSitter/JSON.hs
+++ tree-sitter-json/TreeSitter/JSON.hs
@@ -1,8 +1,13 @@
 module TreeSitter.JSON
 ( tree_sitter_json
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_json
 
 foreign import ccall unsafe "vendor/tree-sitter-json/src/parser.c tree_sitter_json" tree_sitter_json :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-json/json/src/node-types.json"
