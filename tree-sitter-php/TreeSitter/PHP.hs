274968cbb5d51b2552285c5cac8ddd53080a272e
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:45:18 -0500

    create getNodeTypesPath for PHP

--- tree-sitter-php/TreeSitter/PHP.hs
+++ tree-sitter-php/TreeSitter/PHP.hs
@@ -1,8 +1,12 @@
 module TreeSitter.PHP
 ( tree_sitter_php
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
 
 foreign import ccall unsafe "vendor/tree-sitter-php/src/parser.c tree_sitter_php" tree_sitter_php :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-php/typescript/src/node-types.json"
