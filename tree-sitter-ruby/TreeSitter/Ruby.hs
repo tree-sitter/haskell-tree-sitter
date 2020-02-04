fd31f21946be19a448ffe5619e228d9cbe9d821b
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:51:21 -0500

    fix copy-pasta

--- tree-sitter-ruby/TreeSitter/Ruby.hs
+++ tree-sitter-ruby/TreeSitter/Ruby.hs
@@ -10,4 +10,4 @@ import Paths_tree_sitter_ruby
 foreign import ccall unsafe "vendor/tree-sitter-ruby/src/parser.c tree_sitter_ruby" tree_sitter_ruby :: Ptr Language
 
 getNodeTypesPath :: IO FilePath
-getNodeTypesPath = getDataFileName "vendor/tree-sitter-go/go/src/node-types.json"
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-ruby/ruby/src/node-types.json"
