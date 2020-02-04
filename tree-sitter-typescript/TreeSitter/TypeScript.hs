6256c12ef0b96567edf20de134ad5cee70fd6fb0
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:39:50 -0500

    create getNodeTypesPath for typescript

--- tree-sitter-typescript/TreeSitter/TypeScript.hs
+++ tree-sitter-typescript/TreeSitter/TypeScript.hs
@@ -1,8 +1,13 @@
 module TreeSitter.TypeScript
 ( tree_sitter_typescript
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_typescript
 
 foreign import ccall unsafe "vendor/tree-sitter-typescript/typescript/src/parser.c tree_sitter_typescript" tree_sitter_typescript :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-typescript/typescript/src/node-types.json"
