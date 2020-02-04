3c5af65df74715aefb40cabc977630f786e996e8
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:49:49 -0500

    add getNodeTypesPath to Go

--- tree-sitter-go/TreeSitter/Go.hs
+++ tree-sitter-go/TreeSitter/Go.hs
@@ -1,8 +1,13 @@
 module TreeSitter.Go
 ( tree_sitter_go
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_go
 
 foreign import ccall unsafe "vendor/tree-sitter-go/src/parser.c tree_sitter_go" tree_sitter_go :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-go/go/src/node-types.json"
