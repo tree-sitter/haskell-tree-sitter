57a2f186b837cacc2537eb52edf4909fe9228b13
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:50:39 -0500

    add getNodeTypesPath to Ruby

--- tree-sitter-ruby/TreeSitter/Ruby.hs
+++ tree-sitter-ruby/TreeSitter/Ruby.hs
@@ -1,8 +1,13 @@
 module TreeSitter.Ruby
 ( tree_sitter_ruby
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_ruby
 
 foreign import ccall unsafe "vendor/tree-sitter-ruby/src/parser.c tree_sitter_ruby" tree_sitter_ruby :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-go/go/src/node-types.json"
