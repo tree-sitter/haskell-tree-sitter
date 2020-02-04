4e1f6efbad72dea95b06bc27bbeab149d88fb734
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:47:34 -0500

    fix copy-pasta from typescript

--- tree-sitter-php/TreeSitter/PHP.hs
+++ tree-sitter-php/TreeSitter/PHP.hs
@@ -10,4 +10,4 @@ import Paths_tree_sitter_php
 foreign import ccall unsafe "vendor/tree-sitter-php/src/parser.c tree_sitter_php" tree_sitter_php :: Ptr Language
 
 getNodeTypesPath :: IO FilePath
-getNodeTypesPath = getDataFileName "vendor/tree-sitter-php/typescript/src/node-types.json"
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-php/php/src/node-types.json"
