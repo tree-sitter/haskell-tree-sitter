fb1c54d0e19c14d2745b26d060489cc05751ae84
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:59:44 -0500

    add getNodeTypesPath to Haskell

--- tree-sitter-haskell/TreeSitter/Haskell.hs
+++ tree-sitter-haskell/TreeSitter/Haskell.hs
@@ -1,8 +1,13 @@
 module TreeSitter.Haskell
 ( tree_sitter_haskell
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_haskell
 
 foreign import ccall unsafe "vendor/tree-sitter-haskell/src/parser.c tree_sitter_haskell" tree_sitter_haskell :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-haskell/haskell/src/node-types.json"
