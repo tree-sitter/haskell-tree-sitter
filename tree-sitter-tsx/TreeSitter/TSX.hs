0ff80d3b9b70ffe900b62bb8e71d3b3a4a1f608d
Ayman Nadeem <aymannadeem@github.com>
2020-02-04 15:53:40 -0500

    add getNodeTypesPath to TSX

--- tree-sitter-tsx/TreeSitter/TSX.hs
+++ tree-sitter-tsx/TreeSitter/TSX.hs
@@ -1,8 +1,13 @@
 module TreeSitter.TSX
 ( tree_sitter_tsx
+, getNodeTypesPath
 ) where
 
 import Foreign.Ptr
 import TreeSitter.Language
+import Paths_tree_sitter_tsx
 
 foreign import ccall unsafe "vendor/tree-sitter-typescript/tsx/src/parser.c tree_sitter_tsx" tree_sitter_tsx :: Ptr Language
+
+getNodeTypesPath :: IO FilePath
+getNodeTypesPath = getDataFileName "vendor/tree-sitter-tsx/tsx/src/node-types.json"
