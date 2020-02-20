069282dfd9d9215962668f202f3486fbafcd5568
Timothy Clem <timothy.clem@gmail.com>
2020-02-20 10:35:54 -0800

    Package up the test corpus

--- tree-sitter-go/TreeSitter/Go.hs
+++ tree-sitter-go/TreeSitter/Go.hs
@@ -1,6 +1,7 @@
 module TreeSitter.Go
 ( tree_sitter_go
 , getNodeTypesPath
+, getTestCorpusDir
 ) where
 
 import Foreign.Ptr
@@ -11,3 +12,6 @@ foreign import ccall unsafe "vendor/tree-sitter-go/src/parser.c tree_sitter_go" 
 
 getNodeTypesPath :: IO FilePath
 getNodeTypesPath = getDataFileName "vendor/tree-sitter-go/src/node-types.json"
+
+getTestCorpusDir :: IO FilePath
+getTestCorpusDir = getDataFileName "vendor/tree-sitter-go/corpus"
