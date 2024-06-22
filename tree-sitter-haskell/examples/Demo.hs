bfb44b888e8f609a514e7ba7ffe84267a15d4395
Heitor Toledo Lassarote de Paula <heitortoledo@hotmail.com>
2024-06-22 19:17:30 -0300

    Bump submodules and deal with problems
    Problem: It's been a pretty long time since the previous bump, so we'd
    like to do it again, but this time, let's bump everything. This causes
    some trouble that we need to deal with, though.
    
    Solution:
    * Run `git submodule update --remote --merge --init` to bump submodules.
    * After this, running `cabal test all` has one failure. This is because
      the cursor size was increased from 24 to 32, so `sizeOfCursor` was
      updated accordingly. I took the liberty to fix a small typo in its
      documentation as well.
    * After this, I wanted to test tree-sitter. I found a demo in
      `tree-sitter-haskell` but noticed it was SEGFAULTing when running. I
      don't understand the cause, but I assume the Haskell tree-sitter
      grammar changed so this demo was no longer valid. I "fixed" it by
      checking the child at index 2 (bindings) rather than 3, since there is
      no index 3. I took the liberty to modernize it so warnings are gone
      too.
    * After that, I wanted to test a real-world tree-sitter contract. For
      that, I visited a pretty old commit of one of our Haskell projects
      that used tree-sitter (nowadays it doesn't use it anymore), pointed it
      to my local branch of `haskell-tree-sitter`, and tried to build it.
        * First, it failed because `nodeIsMissing` wasn't present.
          Curiously, I couldn't find it in the branch it was previously
          using either, but it may be trivially added to `Node` using the
          `tree-sitter` API if desired, but I didn't bother. I removed its
          usage from the Haskell project and continued with life.
        * It also complained that `tree_sitter/parser.h` was missing.
          Indeed, it was deleted from the tree-sitter repository, so I
          removed it from the Cabal file.
        * With those changes, the project compiled was able to successfully
          parse a test file for CameLIGO.
    * n.b.: I didn't check each tree-sitter-* directory, just the haskell
      one as it had a demo, so it's possible there are other failures I'm
      unaware.

--- tree-sitter-haskell/examples/Demo.hs
+++ tree-sitter-haskell/examples/Demo.hs
@@ -1,34 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
-module Main where
+module Main (main) where
 
 import           TreeSitter.Parser
 import           TreeSitter.Tree
-import           TreeSitter.Language
 import           TreeSitter.Haskell
 import           TreeSitter.Node
 import           Foreign.C.String
-import           Foreign.C.Types
-import           Foreign.Ptr                    ( Ptr(..)
+import           Foreign.Ptr                    ( Ptr
                                                 , nullPtr
-                                                , plusPtr
                                                 )
-import           Foreign.Marshal.Alloc          ( malloc
-                                                , mallocBytes
-                                                )
+import           Foreign.Marshal.Alloc          ( malloc )
 import           Foreign.Marshal.Array          ( mallocArray )
 import           Foreign.Storable               ( peek
                                                 , peekElemOff
                                                 , poke
                                                 )
-import           Foreign.Marshal.Utils          ( new )
 import           Control.Monad
 
 
 main :: IO ()
 main = do
   parser <- ts_parser_new
-  ts_parser_set_language parser tree_sitter_haskell
+  _ <- ts_parser_set_language parser tree_sitter_haskell
 
   let source =
         "module Test (main) where\nimport Lib\nf1 = undefined\nf2 = undefined"
@@ -39,8 +33,8 @@ main = do
   n          <- malloc
   ts_tree_root_node_p tree n
 
-  print "module (root) ------------"
-  n@Node {..} <- peek n
+  putStrLn "module (root) ------------"
+  Node {..} <- peek n  -- header, imports, and declarations
   let childCount = fromIntegral nodeChildCount
 
   children <- mallocArray childCount
@@ -50,8 +44,8 @@ main = do
 
   printChildren children childCount
 
-  print "where ------------"
-  n@Node {..} <- peekElemOff children 3
+  putStrLn "declarations ------------"
+  Node {..} <- peekElemOff children 2  -- declarations: bind and bind
   let nextChildCount = fromIntegral nodeChildCount
 
   nextChildren <- mallocArray nextChildCount
@@ -61,7 +55,7 @@ main = do
 
   printChildren nextChildren nextChildCount
 
-  print "END"
+  putStrLn "END"
 
 printChildren :: Ptr Node -> Int -> IO ()
 printChildren children count = forM_
@@ -78,4 +72,4 @@ printNode n@(Node {..}) = do
       start        = "(" ++ show pointRow ++ "," ++ show pointColumn ++ ")"
   let TSPoint {..} = nodeEndPoint
       end          = "(" ++ show pointRow ++ "," ++ show pointColumn ++ ")"
-  print $ theType ++ start ++ "-" ++ end
+  putStrLn $ theType ++ start ++ "-" ++ end
